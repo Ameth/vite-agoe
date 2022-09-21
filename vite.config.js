07e63d8747e56ef623031abc921f2f77a5129f7d
Ameth Ordoñez <amethgabriel@hotmail.com>
2022-09-21 17:39:31 -0500

    Multi-pages support

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
+import { resolve } from "path";
 
 export default defineConfig(({ command, mode }) => {
   const port = 3030;
@@ -10,13 +11,22 @@ export default defineConfig(({ command, mode }) => {
 
   if (mode === "development") {
     console.log(`${env.VITE_NAME} en modo de desarrollo activado!`);
+    return {
+      // server: {
+      //   port,
+      // },
+    };
   } else {
     console.log(`${env.VITE_NAME} en modo de producción activado!`);
+    return {
+      build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, "index.html"),
+            help: resolve(__dirname, "help", "help.html"),
+          },
+        },
+      },
+    };
   }
-
-  return {
-    // server: {
-    //   port,
-    // },
-  };
 });
